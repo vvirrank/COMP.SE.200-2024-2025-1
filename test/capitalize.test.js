cd67881e0a50e2c492b98b132ca907194fe16e77
Veikko Virrankoski <71337077+vvirrank@users.noreply.github.com>
2025-12-07 01:41:07 +0200

    Fix #8: Add tests for capitalize

---
+++ test/capitalize.test.js
@@ -0,0 +1,21 @@
+import { expect } from "chai"
+import capitalize from "../src/capitalize.js"
+
+describe("capitalizeTests", () => {
+
+    it("should return the input string when already correctly capitalized", () => {
+        expect(capitalize("There is a tree.")).to.equal("There is a tree.");
+    })
+
+    it("should capitalized if not capitalized", () => {
+        expect(capitalize("there is a tree.")).to.equal("There is a tree.");
+    })
+
+    it("should uncapitalized non-starting letters if capitalized", () => {
+        expect(capitalize("thEre Is A TREE.")).to.equal("There is a tree.");
+    })
+
+    it("should capitalize and uncapitalize non-ascii letters", () => {
+        expect(capitalize("äÄh")).to.equal("Ääh");
+    })
+})
